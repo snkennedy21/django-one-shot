f1217f3b35e4a23102b223e8a2124a76740f1a17
Sean Kennedy <snkennedy21@gmail.com>
2022-07-26 11:52:49 -0400

    complete F6

--- todos/models.py
+++ todos/models.py
@@ -7,3 +7,10 @@ class TodoList(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class TodoItem(models.Model):
+  task = models.CharField(max_length=100)
+  due_date = models.DateField(blank=True, null=True)
+  is_completed = models.BooleanField(default=False)
+  list = models.ForeignKey("TodoList", related_name="todo_items", on_delete=models.CASCADE)
